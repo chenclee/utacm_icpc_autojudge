822120e7c2fa9f92b1cff15a9697d69c3e3eb96a
Jonathan Lee <chencjlee@wireless-10-145-132-85.public.utexas.edu>
2014-10-18 23:24:21 -0500

    Initial commit.

---
+++ backend.py
@@ -0,0 +1,77 @@
+import logging
+import time
+import threading
+import os
+import uuid
+import Queue
+
+class Verdict(object):
+    qu = 0 # In Queue
+    ac = 1 # Accepted
+    wa = 2 # Wrong Answer
+    ce = 3 # Compile Error
+    re = 4 # Runtime Error
+    tl = 5 # Time Limit Exceeded
+    ml = 6 # Memory Limit Exceeded
+    ol = 7 # Output Limit Exceeded
+    se = 8 # Submission Error
+    rf = 9 # Restricted Function
+
+
+lang_compile = {'GNU C++ 4': 'g++ -static -fno-optimize-sibling-calls -fno-strict-aliasing -DONLINE_JUDGE -lm -s -x c++ -Wl,--stack=268435456 -O2 -o prog_bin {filename}',
+        'GNU C++11 4': 'g++ -static -fno-optimize-sibling-calls -fno-strict-aliasing -DONLINE_JUDGE -lm -s -x c++ -Wl,--stack=268435456 -O2 -std=c++11 -D__USE_MINGW_ANSI_STDIO=0 -o prog_bin {filename}',
+        'GNU C 4': 'gcc -static -fno-optimize-sibling-calls -fno-strict-aliasing -DONLINE_JUDGE -fno-asm -lm -s -Wl,--stack=268435456 -O2 -o prog_bin {filename}',
+        'Java 6, 7': 'javac -cp ".;*" {filename}',
+        'Python 2.7': ';',
+}
+lang_run = {'GNU C++ 4': './prog_bin',
+        'GNU C++11 4': './prog_bin',
+        'GNU C 4': './prog_bin',
+        'Java 6, 7': 'java -Xmx512M -Xss64M -DONLINE_JUDGE=true -Duser.language=en -Duser.region=US -Duser.variant=US -jar {filename}',
+        'Python 2.7': 'python {filename}',
+}
+
+
+class Submission(object):
+    next_sid = 0
+    @classmethod
+    def get_next_sid(cls):
+        sid = cls.next_sid
+        cls.next_sid += 1
+        return sid
+
+    def __init__(self, pid, uid, lang, filename):
+        self.pid = problem_id
+        self.sid = Submission.get_next_sid()
+        self.uid = uid
+        self.filename = filename
+        self.time = time.time()
+        self.verdict = Verdict.qu
+
+    def __str__(self):
+        return 'pid=%s;sid=%s;uid=%s;time=%s;verdict=%s' % \
+                (self.pid, self.sid, self.uid, self.time, self.verdict.__name__)
+
+
+class Problem(object):
+    def __init__(self, pid, time_limit):
+        self.pid = pid
+        self.time_limit = time_limit
+
+    def judge(self, submission):
+        # TODO(chencjlee): actually implement judging
+        return Verdict.ac
+
+
+class Scoreboard(object):
+    def __init__(self, problems):
+        self.start_time = time.time()
+        self.cache_lock = threading.Lock()
+        self.cache = {}
+
+    def new_submission(self, submission):
+        logging.info('New submission: %s' % submission)
+        judge_queue.put(submission)
+        self.cache_lock.acquire()
+        self.cache[submission.sid] = submission
+        self.cache_lock.release()
---
+++ judge.py
@@ -0,0 +1,117 @@
+#!/usr/bin/python
+
+import logging
+import tornado.auth
+import tornado.escape
+import tornado.ioloop
+import tornado.web
+import time
+import threading
+import os
+import uuid
+import Queue
+
+from tornado.concurrent import Future
+from tornado import gen
+from tornado.options import define, options, parse_command_line
+
+from backend import Scoreboard, Problem, Submission, Verdict
+
+
+define('port', default=8000, help='start on the given port', type=int)
+define('debug', default=False, help='run in debug mode')
+
+
+scoreboard = Scoreboard([])
+judge_queue = Queue.Queue()
+
+
+class BaseHandler(tornado.web.RequestHandler):
+    def get_current_user(self):
+        user_json = self.get_secure_cookie('utacm_contest_user')
+        if not user_json:
+            return None
+        return tornado.escape.json_decode(user_json)
+
+
+class MainHandler(tornado.web.RequestHandler):
+    @tornado.web.authenticated
+    def get(self):
+        self.render('index.html')
+
+
+class SubmitCodeHandler(BaseHandler):
+    @tornado.web.authenticated
+    def post(self):
+        pass
+
+
+class SubmitClarificationHandler(BaseHandler):
+    @tornado.web.authenticated
+    def post(self):
+        pass
+
+
+class ViewScoreboardHandler(BaseHandler):
+    @tornado.web.authenticated
+    @gen.coroutine
+    def post(self):
+        pass
+
+    def on_connection_close(self):
+        pass
+
+
+class ViewSubmissionsHandler(BaseHandler):
+    @tornado.web.authenticated
+    @gen.coroutine
+    def post(self):
+        pass
+
+    def on_connection_close(self):
+        pass
+
+
+class AuthLoginHandler(BaseHandler, tornado.auth.GoogleMixin):
+    @gen.coroutine
+    def get(self):
+        if self.get_argument('openid.mode', None):
+            user = yield self.get_authenticated_user()
+            self.set_secure_cookie('utacm_contest_user',
+                                   tornado.escape.json_encode(user))
+            self.redirect('/')
+            return
+        self.authenticate_redirect(ax_attrs=['name'])
+
+
+class AuthLogoutHandler(BaseHandler):
+    def get(self):
+        self.clear_cookie('utacm_contest_user')
+        self.write("You are now logged out")
+
+
+def main():
+    parse_command_line()
+    app = tornado.web.Application(
+            [
+                (r'/', MainHandler),
+                (r'/auth/login', AuthLoginHandler),
+                (r'/auth/logout', AuthLogoutHandler),
+                (r'/a/submit/code', SubmitCodeHandler),
+                (r'/a/submit/clarification', SubmitClarificationHandler),
+                (r'/a/view/scoreboard', ViewScoreboardHandler),
+                (r'/a/view/submissions', ViewSubmissionsHandler),
+            ],
+            cookie_secret='TODO(chencjlee): Generate a random value',
+            login_url='/auth/login',
+            template_path=os.path.join(os.path.dirname(__file__), 'templates'),
+            static_path=os.path.join(os.path.dirname(__file__), 'static'),
+            xsrf_cookies=True,
+            debug=options.debug,
+    )
+    app.listen(options.port)
+    tornado.ioloop.IOLoop.instance().start()
+
+
+if __name__ == '__main__':
+    main()
