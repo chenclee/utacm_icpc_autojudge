6ea4d1331a4afde8cfc5a2c6e3df0ee80bb29224
Jonathan Lee <chencjlee@wireless-10-147-101-94.public.utexas.edu>
2014-11-06 20:02:33 -0600

    skeleton for refactored site

---
+++ console.py
@@ -0,0 +1,54 @@
+def start_contest(args):
+    pass
+
+
+def add_duration(args):
+    pass
+
+
+def freeze_scoreboard(args):
+    pass
+
+
+def unfreeze_scoreboard(args):
+    pass
+
+
+def view_recent_submissions(args):
+    pass
+
+
+def view_clarification_queue(args):
+    pass
+
+
+def respond_clarification(args):
+    pass
+
+
+if __name__ == '__main__':
+    supported_functions = {
+        'start_contest':
+            (start_contest, 'start [delay] [duration] [prob_dir] [sub_dir]'),
+        'add_duration':
+            (add_duration, 'add_duration [duration]'),
+        'freeze_scoreboard':
+            (freeze_scoreboard, 'freeze_scoreboard'),
+        'unfreeze_scoreboard':
+            (unfreeze_scoreboard, 'unfreeze_scoreboard'),
+        'view_recent_submissions':
+            (view_recent_submissions, 'view_recent_submissions'),
+        'view_clarification_queue':
+            (view_clarification_queue, 'view_clarification_queue'),
+        'respond':
+            (respond_clarification, 'respond [clarif_id] [response]')
+    }
+
+    while True:
+        cmd_line = raw_input().trim()
+        cmd = cmd_line.split()
+        try:
+            supported_functions[cmd[0]][0](*cmd[1:])
+        except Exception:
+            if cmd_line != '' and cmd[0] in supported_functions:
+                print 'Usage:', supported_functions[cmd[0]][1]
---
+++ contest.py
@@ -0,0 +1,27 @@
+class Contest:
+    def __init__(self, delay, duration, problems):
+        pass
+
+    def is_running(self):
+        pass
+
+    def remaining_time(self):
+        pass
+
+    def extend(self):
+        pass
+
+    def scoreboard(self):
+        pass
+
+    def submit_solution(self, user_id, problem_id, passed):
+        pass
+
+    def clarifs(self):
+        pass
+
+    def submit_clarif(self, message):
+        pass
+
+    def respond_clarif(self, clarif_id, response):
+        pass
